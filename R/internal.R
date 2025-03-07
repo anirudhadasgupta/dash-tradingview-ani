0cba545183db8f15dde9f67eaa0495ef22c3870a
anirudhadasgupta <122409207+anirudhadasgupta@users.noreply.github.com>
2025-03-07 04:25:04 -0600

    Update internal.R

--- R/internal.R
+++ R/internal.R
@@ -2,13 +2,13 @@
 deps_metadata <- list(`dash_tvlwc` = structure(list(name = "dash_tvlwc",
 version = "0.1.1", src = list(href = NULL,
 file = "deps"), meta = NULL,
-script = 'dash_tvlwc.min.js',
+script = 'dash_tvlwc.dev.js',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashTvlwc",
 all_files = FALSE), class = "html_dependency"),
 `dash_tvlwc` = structure(list(name = "dash_tvlwc",
 version = "0.1.1", src = list(href = NULL,
 file = "deps"), meta = NULL,
-script = 'dash_tvlwc.min.js.map',
+script = 'dash_tvlwc.dev.js.map',
 stylesheet = NULL, head = NULL, attachment = NULL, package = "dashTvlwc",
 all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
 return(deps_metadata)
